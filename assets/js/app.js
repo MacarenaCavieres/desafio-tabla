595c8ee68325bbe78741b73971955bf5708852a4
MCavieres <maca.c.91@gmail.com>
2024-03-05 21:22:15 -0300

    hasta pregunta 3 de desafio listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -192,3 +192,88 @@ dental.forEach((item) => {
     <td>${item.prevision}</td> 
 </tr>`;
 });
+
+//-------------------pregunta1-------------------------------------------------------
+
+const traumatologiaAgregados = [
+    {
+        hora: "09:00",
+        especialista: "RENÉ POBLETE",
+        paciente: "ANA GELLONA",
+        rut: "13123329-7",
+        prevision: "ISAPRE",
+    },
+    {
+        hora: "09:30",
+        especialista: "MARIA SOLAR",
+        paciente: "RAMIRO ANDRADE",
+        rut: "12221451-K",
+        prevision: "FONASA",
+    },
+    {
+        hora: "10:00",
+        especialista: "RAUL LOYOLA",
+        paciente: "CARMEN ISLA",
+        rut: "10112348-3",
+        prevision: "ISAPRE",
+    },
+    {
+        hora: "10:30",
+        especialista: "ANTONIO LARENAS",
+        paciente: "PABLO LOAYZA",
+        rut: "13453234-1",
+        prevision: "ISAPRE",
+    },
+    {
+        hora: "12:00",
+        especialista: "MATIAS ARAVENA",
+        paciente: "SUSANA POBLETE",
+        rut: "14345656-6",
+        prevision: "FONASA",
+    },
+];
+
+traumatologiaAgregados.forEach((object) => {
+    traumatologia.push(object);
+});
+
+tableTraumatologia.innerHTML = "";
+
+traumatologia.forEach((item) => {
+    tableTraumatologia.innerHTML += `<tr>
+    <td>${item.hora}</td>
+    <td>${item.especialista}</td>
+    <td>${item.paciente}</td>
+    <td>${item.rut}</td>
+    <td>${item.prevision}</td> 
+</tr>`;
+});
+
+//----------------pregunta2------------------------------------------------------------
+
+radiologia.pop();
+radiologia.shift();
+
+tableRadiologia.innerHTML = "";
+
+radiologia.forEach((item) => {
+    tableRadiologia.innerHTML += `<tr>
+    <td>${item.hora}</td>
+    <td>${item.especialista}</td>
+    <td>${item.paciente}</td>
+    <td>${item.rut}</td>
+    <td>${item.prevision}</td>
+    </tr>`;
+});
+
+//----------------pregunta3------------------------------------------------------------
+
+const dentalSinTable = document.querySelector("#dentalSinTable");
+
+radiologia.forEach((item) => {
+    dentalSinTable.innerHTML += `<p>${item.hora} - ${item.especialista} - ${item.especialista} - ${item.paciente} - ${item.rut} - ${item.prevision}</p> `;
+});
+
+//----------------pregunta4------------------------------------------------------------
+
+const listadoPacientes = document.querySelector("#listadoPacientes");
